0facd2f8165a90e1933f407b73a8f764c85e65bd
Bryan Paronto <bparonto@gmail.com>
2017-07-29 20:07:14 -0700

    adds doc block. purty

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -1,13 +1,31 @@
 const Post = require('../models/post')
 
 class PostsController {
-  
+  /**
+   * List all posts
+   * @param  {Object}   req  Express request object
+   * @param  {Object}   res  Express response object
+   * @param  {Function} next Call next in middleware chain
+   * @return {Promise}        Resolves with list of posts
+   */
   index (req, res, next) {
+    const limit = req.query.limit || 10 
+    const skip = req.query.skip || 0 
+
     Post.find({})
+      .limit(limit)
+      .skip(skip)
       .then((response) => res.send(response))
       .catch(next)
   }
 
+  /**
+   * Creates a new post and returns it
+   * @param  {Object}   req  Express request object
+   * @param  {Object}   res  Express response object
+   * @param  {Function} next Call next in middleware chain
+   * @return {Promise}       Resolves with newly create post
+   */
   create (req, res, next) {
     const postProps = req.body
     Post.create(postProps)
@@ -15,6 +33,13 @@ class PostsController {
      .catch(next)
   }
 
+  /**
+   * Queries a single post by id
+   * @param  {Object}   req  Express request object
+   * @param  {Object}   res  Express response object
+   * @param  {Function} next Call next in middleware chain
+   * @return {Promise}       Resolves with the queried post
+   */
   read (req, res, next) {
     const { id } = req.params
 
@@ -23,6 +48,13 @@ class PostsController {
       .catch(next)
   }
 
+  /**
+   * Updates a post with the given id
+   * @param  {Object}   req  Express request object
+   * @param  {Object}   res  Express response object
+   * @param  {Function} next Call next in middleware chain
+   * @return {Promise}       Resolves with updated post
+   */
   update (req, res, next) {
     const { id } = req.params
     const postProps = req.body
@@ -33,6 +65,13 @@ class PostsController {
       .catch(next)
   }
 
+  /**
+   * Deletes a post with the given id
+   * @param  {Object}   req  Express request object
+   * @param  {Object}   res  Express response object
+   * @param  {Function} next Call next in middleware chain
+   * @return {Promise}       Resolves with deleted post
+   */
   delete (req, res, next) {
     const { id } = req.params
     Post.findByIdAndRemove(id)
